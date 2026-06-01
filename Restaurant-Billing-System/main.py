c05661cae100f4315577e1e435f6cdb86e04ba66
anuuujjj <anuj.4027g@gmail.com>
2026-06-01 20:30:36 +0530

    Add Restaurant Billing System project

---
+++ Restaurant-Billing-System/main.py
@@ -0,0 +1,14 @@
+# Assignment 1 (01/06/2026)
+
+# Restaurant Billing System
+
+def show_menu():
+    print("\nWelcome!, Please choose your order from the menu.")
+    print("\n               ...MENU...")
+    print("1. Burger       -       Price: Rs.89")
+    print("2. Pizza        -       Price: Rs.299")
+    print("3. Sandwich     -       Price: Rs.49")
+    print("4. Aloo Paratha -       Price: Rs.30")
+    print("5. Rice Dal     -       Price: Rs.99")
+    print("6. Paneer Roll. -       Price: Rs.75")
+    print("7. Checkout.")
